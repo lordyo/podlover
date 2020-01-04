d9c33ebb612ac90c271707c5143ef8fe4d958434
Timm Suess <github@sporez.com>
2020-01-04 18:37:29 +0100

    new function create_example()

---
+++ R/podlove_create_example.R
@@ -0,0 +1,71 @@
+#' Create a List of Download Example Tables
+#'
+#' This is a wrapper function to easily create example podcast download table. 
+#'
+#' @param n_posts Number of posts to generate (defaults to 15)
+#' @param n_episodes Number of episodes to generate (defaults to 12, must be 
+#'     lower or equal than n_posts)
+#' @param n_useragents Number of useragents to sample from (defaults to 100) 
+#' @param start_date Podcast start date
+#' @param end_date Podcast end date
+#' @param runtime minimum runtime of an episode in days. This parameter defines 
+#'     the time difference between the release of the last episode and the last
+#'     download to be generated.
+#' @param total_dls Total downloads to be generate. Defaults to 5000. Approximate value.
+#' @param seed parameter to fix randomization via \code{set.seed()}
+#' 
+#' @return A list of 5 named tables 
+#'  
+#' @examples 
+#' # create tables for ~10000 downloads
+#' test_list <- podlove_create_example(total_dls = 10000)
+#' 
+#' @export
+
+podlove_create_example <- function(n_posts = 15,
+                                   n_episodes = 10,
+                                   n_useragents = 100,
+                                   start_date = "2019-01-01",
+                                   end_date = "2019-12-31",
+                                   runtime = 30,
+                                   total_dls = 5000,
+                                   seed = NULL) {
+  # set seed if given
+  if (!is.null(seed)) set.seed(seed)
+  
+  total_runtime <- lubridate::interval(lubridate::ymd(start_date), lubridate::ymd(end_date)) / lubridate::days(1)
+  dl_per_ep_day <- (2 * total_dls) / (n_episodes * total_runtime) 
+  
+  
+  posts <- podlove_create_example_posts(n_posts,
+                                        start_date,
+                                        end_date,
+                                        seed = seed)
+  
+  episodes <- podlove_create_example_episodes(n_episodes, 
+                                              posts,
+                                              seed = seed)
+  
+  mediafiles <- podlove_create_example_mediafiles(episodes,
+                                                  seed = seed)
+  
+  useragents <- podlove_create_example_useragents(n_useragents,
+                                                  seed = seed)
+  
+  downloads <- podlove_create_example_downloads(posts,
+                                                episodes,
+                                                mediafiles,
+                                                useragents,
+                                                runtime = runtime, 
+                                                dl_per_ep_day = dl_per_ep_day,
+                                                seed = seed)
+  
+  out_tables <- list(posts = posts,
+                     episodes = episodes,
+                     mediafiles = mediafiles,
+                     useragents = useragents,
+                     downloads = downloads)
+  
+  out_tables
+  
+}
