f1fb542cdc5c1a61c529aee1fa01f3a8f3a487d2
Timm Suess <catchall@sporez.com>
2020-02-04 22:04:20 +0100

    Add summary function to plot_curves()
    The sum_fun option in podlove_plot_curves() allows to display a smoothed summary function curve over all curve plots (line plots only).

--- R/podlove_plot_curves.R
+++ R/podlove_plot_curves.R
@@ -28,6 +28,10 @@
 #'     or \code{ggridges::geom_density_ridges}.
 #' @param last_n Number of most recent episodes to filter for. Defaults to 0 
 #'     (no filtering), use negative numbers to filter for first n episodes. 
+#' @param sum_fun Summary function used to draw a (smoothed) curve over all 
+#'     \code{gvar} curves, e.g. \code{mean} or \code{median}. The summary is
+#'     taken from all curves in the dataset, even if a \code{last_n} parameter 
+#'     is set. Do not quote. 
 #' 
 #' @return A ggplot object
 #' 
@@ -37,7 +41,17 @@
 #' 
 #' podlove_plot_curves(dls, gvar = title, relative = TRUE, cumulative = TRUE)
 #' 
-#' # see podlove_graph_download_curves() for more information.
+#' # add a mean curve
+#' podlove_plot_curves(dls, gvar = title, relative = TRUE, cumulative = TRUE,
+#'                     sum_fun = mean)
+#'      
+#' # show only the last 4 episodes' curves. Note how the summary curve remains 
+#' # the same as in the plot before.
+#' podlove_plot_curves(dls, gvar = title, relative = TRUE, cumulative = TRUE,
+#'                     sum_fun = mean, last_n = 4)
+#'                     sum_fun = mean, last_n = 4)
+#' 
+#' # see podlove_graph_download_curves() for additional information.
 #' 
 #' @importFrom magrittr %>% 
 #' 
@@ -55,21 +69,52 @@ podlove_plot_curves <- function(dldata,
 																labelmethod = "last.points",
 																printout = TRUE,
 																last_n = 0,
+																sum_fun = NULL,
 																...) {
   
+	# prepare data
 	g_data <- podlove_prepare_stats_for_graph(df_stats = dldata,
 																						gvar = {{gvar}},
 																						hourly = hourly,
 																						relative = relative, 
 																						last_n = last_n)
 	
+	# plot curves
 	g <- podlove_graph_download_curves(df_tidy_data = g_data,
 																		 gvar = {{gvar}},
 																		 cumulative = cumulative,
 																		 plot_type = plot_type,
 																		 labelmethod = labelmethod,
-																		 printout = printout,
+																		 printout = FALSE,
 																		 ...)
+	
+	# add summary function to plot
+	
+	if (!is.null(sum_fun) && plot_type == "ridge") {
+		warning("Option sum_fun ignored, as it can't be used with ridge plots.")
+		
+	} else if (!is.null(sum_fun) && !is.function(sum_fun)) {
+		warning(paste0("Option sum_fun =  '", sum_fun, "' ignored: Not a function."))
+		
+	} else if (is.function(sum_fun)) {
+		
+		sumdata <- podlove_prepare_stats_for_graph(df_stats = dldata,
+																							 gvar = {{gvar}},
+																							 hourly = hourly,
+																							 relative = relative)
+		
+		sumdata <- sumdata %>% 
+			group_by(time) %>% 
+			dplyr::summarise_at(c("listeners", "listeners_total"), sum_fun)
+		
+		g <- g + ggplot2::geom_smooth(data = sumdata, 
+																	ggplot2::aes(color = NULL), 
+																	color = "#333333", size = 0.5,
+																	method = "loess")
+		
+	}
+	
+	if (printout) print(g)
 	
 	g
 }
