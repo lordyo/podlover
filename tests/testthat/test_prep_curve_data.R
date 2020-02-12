fcefdd3351d69e76f5f6a4a69386b269465abfb8
Timm Suess <catchall@sporez.com>
2020-02-12 22:11:37 +0100

    unit tests: reflect argument change in curve plots
    includes new vdiffr reference images (slight deviations due to other rounding rules)

--- tests/testthat/test_prep_curve_data.R
+++ tests/testthat/test_prep_curve_data.R
@@ -6,85 +6,85 @@ t_exmpl <- t_exmpl
 
 # simple totals
 t_prep_11 <- podlove_prepare_stats_for_graph(df_stats = t_exmpl, 
-																					hourly = FALSE, 
+																					time_unit = "days", 
 																					relative = TRUE)
 
 t_prep_12 <- podlove_prepare_stats_for_graph(df_stats = t_exmpl, 
-																					hourly = FALSE, 
+																					time_unit = "days", 
 																					relative = FALSE)
 
 t_prep_13 <- podlove_prepare_stats_for_graph(df_stats = t_exmpl, 
-																					hourly = TRUE, 
+																					time_unit = "hours", 
 																					relative = TRUE)
 
 t_prep_14 <- podlove_prepare_stats_for_graph(df_stats = t_exmpl, 
-																					hourly = TRUE, 
+																					time_unit = "hours", 
 																					relative = FALSE)
 
 # gvar = episode related
 t_prep_21 <- podlove_prepare_stats_for_graph(df_stats = t_exmpl,
 																						 gvar = title, 
-																					hourly = FALSE, 
+																					time_unit = "days", 
 																					relative = TRUE)
 
 t_prep_22 <- podlove_prepare_stats_for_graph(df_stats = t_exmpl,
 																						 gvar = title, 
-																					hourly = FALSE, 
+																					time_unit = "days", 
 																					relative = FALSE)
 
 t_prep_23 <- podlove_prepare_stats_for_graph(df_stats = t_exmpl, 
 																						 gvar = title, 
-																					hourly = TRUE, 
+																					time_unit = "hours", 
 																					relative = TRUE)
 
 t_prep_24 <- podlove_prepare_stats_for_graph(df_stats = t_exmpl, 
 																						 gvar = title, 
-																					hourly = TRUE, 
+																					time_unit = "hours", 
 																					relative = FALSE)
 
 # gvar = other variable
 t_prep_31 <- podlove_prepare_stats_for_graph(df_stats = t_exmpl,
 																						 gvar = context, 
-																					hourly = FALSE, 
+																					time_unit = "days", 
 																					relative = TRUE)
 
 t_prep_32 <- podlove_prepare_stats_for_graph(df_stats = t_exmpl,
 																						 gvar = context, 
-																					hourly = FALSE, 
+																					time_unit = "days", 
 																					relative = FALSE)
 
 t_prep_33 <- podlove_prepare_stats_for_graph(df_stats = t_exmpl, 
 																						 gvar = context, 
-																					hourly = TRUE, 
+																					time_unit = "hours", 
 																					relative = TRUE)
 
 t_prep_34 <- podlove_prepare_stats_for_graph(df_stats = t_exmpl, 
 																						 gvar = context, 
-																						 hourly = TRUE, 
+																						 time_unit = "hours", 
 																						 relative = FALSE)
 
 # gvar = episode related + last_n
 t_prep_41 <- podlove_prepare_stats_for_graph(df_stats = t_exmpl,
 																						 gvar = title, 
-																					hourly = FALSE, 
+																					time_unit = "days", 
 																					relative = TRUE,
 																					last_n = 3)
 
 t_prep_42 <- podlove_prepare_stats_for_graph(df_stats = t_exmpl,
 																						 gvar = title, 
-																					hourly = FALSE, 
+																					time_unit = "days", 
 																					relative = FALSE,
 																					last_n = 3)
 
 t_prep_43 <- podlove_prepare_stats_for_graph(df_stats = t_exmpl, 
 																						 gvar = title, 
-																					hourly = TRUE, 
+																					time_unit = "hours", 
 																					relative = TRUE,
 																					last_n = 3)
 
 t_prep_44 <- podlove_prepare_stats_for_graph(df_stats = t_exmpl, 
 																						 gvar = title, 
-																					hourly = TRUE, 
+																					time_unit = "hours", 
 																					relative = FALSE,
 																					last_n = 3)
 
@@ -98,7 +98,7 @@ test_that("prep data without gvar have correct dimensions", {
 	expect_equal(ncol(t_prep_14), 3)
 	
 	expect_equal(nrow(t_prep_11), 310)
-	expect_equal(nrow(t_prep_12), 355)
+	expect_equal(nrow(t_prep_12), 353)
 	expect_equal(nrow(t_prep_13), 1525)
 	expect_equal(nrow(t_prep_14), 1987)
 	
@@ -136,7 +136,7 @@ test_that("prep data without gvar have col classes", {
 	expect_is(t_prep_11$listeners, "integer")
 	expect_is(t_prep_11$listeners_total, "integer")
 	
-	expect_is(t_prep_12$time, "Date")
+	expect_is(t_prep_12$time, "POSIXct")
 	expect_is(t_prep_12$listeners, "integer")
 	expect_is(t_prep_12$listeners_total, "integer")
 	
@@ -160,7 +160,7 @@ test_that("prep data with gvar=title have correct dimensions", {
 	expect_equal(ncol(t_prep_24), 4)
 	
 	expect_equal(nrow(t_prep_21), 816)
-	expect_equal(nrow(t_prep_22), 823)
+	expect_equal(nrow(t_prep_22), 810)
 	expect_equal(nrow(t_prep_23), 2173)
 	expect_equal(nrow(t_prep_24), 2172)
 	
@@ -206,7 +206,7 @@ test_that("prep data with gvar=title have correct dimensions", {
 	expect_equal(ncol(t_prep_34), 4)
 	
 	expect_equal(nrow(t_prep_31), 687)
-	expect_equal(nrow(t_prep_32), 830)
+	expect_equal(nrow(t_prep_32), 827)
 	expect_equal(nrow(t_prep_33), 1901)
 	expect_equal(nrow(t_prep_34), 2574)
 	
@@ -252,7 +252,7 @@ test_that("prep data with gvar=title and last_n have correct dimensions", {
 	expect_equal(ncol(t_prep_44), 4)
 	
 	expect_equal(nrow(t_prep_41), 88)
-	expect_equal(nrow(t_prep_42), 92)
+	expect_equal(nrow(t_prep_42), 89)
 	expect_equal(nrow(t_prep_43), 247)
 	expect_equal(nrow(t_prep_44), 249)
 	
